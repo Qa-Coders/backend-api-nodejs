5073a4a9d22d9d84119a0b82e168e5b1aa2e0338
wasilva <contato.wasilva@hotmail.com>
2023-01-26 19:31:15 -0300

    Ajuste conexão banco

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ if (args.production)
     module.exports = mongoose.connect('mongodb://nome_banco:senha@servidor.com.br:27017/usuario' ) // Umbler
 else
     mongoose.set('strictQuery', false)
-    module.exports = mongoose.connect('mongodb://localhost/banco_dados', {useNewUrlParser: true})    
+    module.exports = mongoose.connect('mongodb://172.0.0.1:27017/banco_dados', {useNewUrlParser: true})    
 
 mongoose.Error.messages.general.required = "O campo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{PATH}' informado é menor que o limite mínimo de '{MIN}'."
